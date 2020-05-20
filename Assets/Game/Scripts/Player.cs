6ae3ba8b03346bf496855262446fa08e9360d2f3
Fahad Shaikh <56800738+fahadshaikh99@users.noreply.github.com>
2020-05-21 01:19:09 +0500

    added speed_up, Enemy and Triple_shot

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -6,7 +6,18 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
+    private GameObject _tripleshotPrefab;
+
+    [SerializeField]
     private GameObject _laserPrefab;
+
+    public bool isSpeedBoostActive = false;
+    
+    
+   
+
+    [SerializeField]
+    public bool TripleShoot = false;
 
     [SerializeField]
     private float _fireRate = 0.25f;
@@ -29,6 +40,7 @@ public class Player : MonoBehaviour
         {
             if (Time.time > _canFire)
             {
+
                 shoot();   
             }
         }
@@ -36,7 +48,15 @@ public class Player : MonoBehaviour
 
     private void shoot()
     {
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+        if (TripleShoot == true)
+        {
+            Instantiate(_tripleshotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+           
+        }
         _canFire = Time.time + _fireRate;
     }
 
@@ -44,9 +64,17 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float VerticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime * _speed * horizontalInput);
-        transform.Translate(Vector3.up * Time.deltaTime * _speed * VerticalInput);
 
+        if (isSpeedBoostActive == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 5.0f * _speed * horizontalInput);
+            transform.Translate(Vector3.up * Time.deltaTime * 5.0f * _speed * VerticalInput);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _speed * horizontalInput);
+            transform.Translate(Vector3.up * Time.deltaTime * _speed * VerticalInput);
+        }
 
         if (transform.position.y < -3.9f)
         {
@@ -65,5 +93,29 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(9.39f, transform.position.y, 0);
         }
+    }
+
+    public void SpeedBoostPowerupOn ()
+    {
+        isSpeedBoostActive = true;
+        StartCoroutine(SpeedBoostDownRoutine());
+    }
+
+    public void TripleShotPowerupOn()
+    {
+        TripleShoot = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+
+    public IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        TripleShoot = false;
+    }
+
+    public IEnumerator SpeedBoostDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        isSpeedBoostActive = false;
     }
 }
---
+++ Assets/Game/Scripts/Powerup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private int powerupID; // 0 = tripleShot, 1 = SpeedUp, 2 = sheilds
+    [SerializeField]
+    private float _speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collided with :" + other.name);
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                if (powerupID == 0)
+                {
+                    player.TripleShotPowerupOn();
+                }
+
+                else if(powerupID == 1)
+                {
+                    player.SpeedBoostPowerupOn();
+                }
+                else if(powerupID == 2)
+                {
+                    //enable power sheild
+                }
+            }
+
+            Destroy(this.gameObject);
+
+        }
+    }
+}
